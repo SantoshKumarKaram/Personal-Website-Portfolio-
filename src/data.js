8e6f61212baea73a4e26263dc9a0a4d9c1c0f3e6
santoshkumar <190301120040@cutm.ac.in>
2023-03-23 15:54:56 +0530

    uploaded all the files

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Crypto-Currency Price Prediction",
+    subtitle: "Meachine Learning",
+    description:
+      "Predicts the bitcoin price for next year.",
+    image: "./project1.png",
+    link: "",
+  },
+  {
+    title: "Student Managment System",
+    subtitle: "C++",
+    description:
+      "Manages the basic student details.",
+      image: "./project2.png",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Employee Leave Managment System",
+    subtitle: "Web base Application",
+    description:
+      "Maintain the records of employee  using html,css,mysql database",
+    image: "./project3.png",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "video-downloader app",
+    subtitle: "Android appication",
+    description:
+      "Android base appication for downloading videos for streaming websites.",
+    image: "./project4.png",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "we provides service to create your own andriod app.",
+    image: "./android.png",
+    name: "App Devlopment",
+    company: "Android",
+  },
+  {
+    quote:
+      "We can devlope a complete Responsive Website.",
+    image: "./web.png",
+    name: "Web Desining",
+    company: "webapplication",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Css",
+  "Html",
+  "Java",
+  "python",
+];
